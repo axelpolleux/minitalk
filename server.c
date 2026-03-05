d161373d1a76658acc6242488ee81531120062c2
apolleux <polleuxaxel@gmail.com>
2026-03-05 18:28:35 +0100

    fix: changed signal handler

--- server.c
+++ server.c
@@ -6,21 +6,20 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:24:36 by apolleux          #+#    #+#             */
-/*   Updated: 2026/03/05 18:12:56 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/03/05 18:26:51 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 #include "minitalk.h"
+#include <signal.h>
 
-void	usr1()
+void	handle_signal(int signal)
 {
-	ft_printf("0");
-}
-
-void	usr2()
-{
-	ft_printf("1");
+	if (signal == SIGUSR1)
+		ft_printf("0");
+	else
+		ft_printf("1");
 }
 
 int	main(void)
@@ -29,8 +28,8 @@ int	main(void)
 	
 	pid = getpid();
 	ft_printf("Process ID: %d\n", pid);
-	signal(SIGUSR1, usr1);
-	signal(SIGUSR2, usr2);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	while (1)
 		pause();
 	return (0);
