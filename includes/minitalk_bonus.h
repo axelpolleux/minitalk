dc6390d2b183645bdcfb95623494d7e5474fd1b0
apolleux <polleuxaxel@gmail.com>
2026-03-07 16:39:09 +0100

    fix: added correct name in header file

--- includes/minitalk_bonus.h
+++ includes/minitalk_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:24:57 by apolleux          #+#    #+#             */
-/*   Updated: 2026/03/05 16:21:01 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/03/07 16:38:50 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
